5c596c20e43c2cc2ccdd0ab1e6df94e9e1be8b4f
Prajon-Adhikari <prajonadhikari44@gmail.com>
2025-11-29 15:47:36 +0545

    removing prisma

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,37 +1,37 @@
-import { prisma } from "@/lib/prisma";
-import bcrypt from "bcryptjs";
+// import { prisma } from "@/lib/prisma";
+// import bcrypt from "bcryptjs";
 
-export async function GET() {
-  try {
-    const users = await prisma.user.findMany();
-    return new Response(JSON.stringify(users), { status: 200 });
-  } catch (error) {
-    console.log("Internal error while fetching users data", error);
-    return new Response(JSON.stringify({ error: "Failed to fetch users" }), {
-      status: 500,
-    });
-  }
-}
+// export async function GET() {
+//   try {
+//     const users = await prisma.user.findMany();
+//     return new Response(JSON.stringify(users), { status: 200 });
+//   } catch (error) {
+//     console.log("Internal error while fetching users data", error);
+//     return new Response(JSON.stringify({ error: "Failed to fetch users" }), {
+//       status: 500,
+//     });
+//   }
+// }
 
-export async function POST(req: Request) {
-  try {
-    const body = await req.json();
+// export async function POST(req: Request) {
+//   try {
+//     const body = await req.json();
 
-    const hashedPassword = await bcrypt.hash(body.password, 10);
+//     const hashedPassword = await bcrypt.hash(body.password, 10);
 
-    const newUser = await prisma.user.create({
-      data: {
-        fullName: body.fullName,
-        email: body.email,
-        password: hashedPassword,
-      },
-    });
+//     const newUser = await prisma.user.create({
+//       data: {
+//         fullName: body.fullName,
+//         email: body.email,
+//         password: hashedPassword,
+//       },
+//     });
 
-    return new Response(JSON.stringify(newUser), { status: 200 });
-  } catch (error) {
-    console.log("Internal error while posting user data", error);
-    return new Response(JSON.stringify({ error: "Failed to post user data" }), {
-      status: 500,
-    });
-  }
-}
+//     return new Response(JSON.stringify(newUser), { status: 200 });
+//   } catch (error) {
+//     console.log("Internal error while posting user data", error);
+//     return new Response(JSON.stringify({ error: "Failed to post user data" }), {
+//       status: 500,
+//     });
+//   }
+// }
