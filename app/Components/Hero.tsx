ff9278f654de647ef9bdd1d2dff3cb1fe9887985
Prajon-Adhikari <prajonadhikari44@gmail.com>
2025-12-05 11:41:11 +0545

    changing hero section

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -3,124 +3,27 @@
 export default function Hero() {
   return (
     <div
-      className="relative overflow-hidden py-[118px]"
+      className="relative overflow-hidden py-[118px] h-screen flex justify-center "
       style={{
-        background: "linear-gradient(135deg, #1f1e1e, #e4302f)",
+        backgroundImage: `url("/heroImage.png")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
-      {/* Animated Background */}
-      <div
-        className="absolute inset-0 z-0"
-        style={{
-          background: `
-            radial-gradient(circle at 20% 50%, rgba(233, 30, 99, 0.12), transparent 60%),
-            radial-gradient(circle at 80% 80%, rgba(255,255,255,0.1), transparent 60%)
-          `,
-          animation: "bgMove 10s ease-in-out infinite alternate",
-        }}
-      />
-
-      {/* Container */}
-      <div className="relative z-10 grid grid-cols-1 md:grid-cols-2 gap-16 ml-25 mr-10 h-[66vh]">
-        
-        {/* Content */}
-        <div className="flex flex-col justify-center">
-          <h1
-            className="opacity-0 translate-y-5 animation-fadeUp text-[3.8rem] font-extrabold leading-tight text-white mb-6"
-          >
-            Fastest{" "}
-            <span
-              className="bg-linear-to-br from-[#e4302f] to-[#f16868] bg-clip-text text-transparent"
-            >
-              Delivery
-            </span>{" "}
-            In The Town
-          </h1>
-
-          <p
-            className="opacity-0 translate-y-5 animation-fadeUpDelay text-gray-200 text-[1rem] leading-7 max-w-[540px] mb-8"
-          >
-            Transform your organization with a centralized, intelligent
-            attendance solution. Track, manage, and analyze attendance with
-            powerful QR technology and real-time insights.
-          </p>
-
-          <div className="flex gap-4 flex-wrap opacity-0 translate-y-5 animation-fadeUpDelay2">
-            <a
-              href="#trial"
-              className="px-6 py-3 text-sm font-semibold text-white bg-[#e4302f] shadow-lg shadow-pink-500/40 hover:bg-red-700 hover:shadow-pink-500/50 rounded-md transition"
-            >
-              Start 14-Day Free Trial
-            </a>
-            <a
-              href="#features"
-              className="px-6 py-3 text-sm text-white border border-white/20 bg-white/10 backdrop-blur-md hover:border-[#e4302f] hover:bg-[#e4302f]/10 rounded-md transition"
-            >
-              Explore Features
-            </a>
-          </div>
-        </div>
-
-        {/* Hero Images */}
-        <div className="relative">
-          <img
-            src="/mobileImageTransparent.png"
-            className="absolute top-0 right-0 z-10 w-[500px] rotate-6 animate-float"
-          />
-          <img
-            src="/mobileImageTransparent3.png"
-            className="absolute -bottom-20 right-60 w-[450px] animate-bounceSlow"
-          />
-        </div>
+      <div className="w-120 text-center mt-10 ">
+        <p className="pt-4 font-semibold text-amber-800">We Offer You</p>
+        <h2 className="font-bold text-6xl text-amber-900">
+          Freshly Baked, Just For You
+        </h2>
+        <p className="pt-4 font-semibold text-amber-800">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          maiores id dignissimos eum voluptate alias nulla, doloremque in quos
+          est.
+        </p>
+        <button className="uppercase bg-yellow-500 text-white px-4 py-1.5 rounded-2xl mt-4">
+          Order Now
+        </button>
       </div>
-
-      {/* Keyframes */}
-      <style jsx>{`
-        @keyframes bgMove {
-          0% {
-            transform: scale(1) translate(0, 0);
-          }
-          100% {
-            transform: scale(1.1) translate(5%, 5%);
-          }
-        }
-
-        .animation-fadeUp {
-          animation: fadeUp 1s ease forwards;
-        }
-        .animation-fadeUpDelay {
-          animation: fadeUp 1s ease forwards;
-          animation-delay: 0.3s;
-        }
-        .animation-fadeUpDelay2 {
-          animation: fadeUp 1s ease forwards;
-          animation-delay: 0.6s;
-        }
-
-        @keyframes fadeUp {
-          to {
-            opacity: 1;
-            transform: translateY(0);
-          }
-        }
-
-        @keyframes float {
-          0%, 100% { transform: translateY(0) rotate(6deg); }
-          50% { transform: translateY(-10px) rotate(6deg); }
-        }
-        .animate-float {
-          animation: float 4s ease-in-out infinite;
-        }
-
-        @keyframes bounceSlow {
-          0%, 100% { transform: translateY(0); }
-          50% { transform: translateY(-12px); }
-        }
-        .animate-bounceSlow {
-          animation: bounceSlow 4s ease-in-out infinite;
-          animation-delay: 2s;
-        }
-      `}</style>
     </div>
   );
 }
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,37 +1,49 @@
-// import { prisma } from "@/lib/prisma";
-// import bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
+import clientPromise from "@/lib/mongodb";
+import { NextResponse } from "next/server";
 
-// export async function GET() {
-//   try {
-//     const users = await prisma.user.findMany();
-//     return new Response(JSON.stringify(users), { status: 200 });
-//   } catch (error) {
-//     console.log("Internal error while fetching users data", error);
-//     return new Response(JSON.stringify({ error: "Failed to fetch users" }), {
-//       status: 500,
-//     });
-//   }
-// }
+export async function GET() {
+  try {
+    const client = await clientPromise;
+    const db = client.db("mydb");
 
-// export async function POST(req: Request) {
-//   try {
-//     const body = await req.json();
+    const users = await db.collection("users").find().toArray();
 
-//     const hashedPassword = await bcrypt.hash(body.password, 10);
+    return NextResponse.json(users);
+  } catch (error) {
+    console.log("Internal error while fetching users data", error);
+    return new Response(JSON.stringify({ error: "Failed to fetch users" }), {
+      status: 500,
+    });
+  }
+}
 
-//     const newUser = await prisma.user.create({
-//       data: {
-//         fullName: body.fullName,
-//         email: body.email,
-//         password: hashedPassword,
-//       },
-//     });
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
 
-//     return new Response(JSON.stringify(newUser), { status: 200 });
-//   } catch (error) {
-//     console.log("Internal error while posting user data", error);
-//     return new Response(JSON.stringify({ error: "Failed to post user data" }), {
-//       status: 500,
-//     });
-//   }
-// }
+    const client = await clientPromise;
+    const db = client.db("mydb");
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+
+    // Save user into MongoDB
+    const result = await db.collection("users").insertOne({
+      fullName: body.fullName,
+      email: body.email,
+      password: hashedPassword,
+      createdAt: new Date(),
+    });
+
+    return NextResponse.json(
+      { success: true, id: result.insertedId },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log("Internal error while posting user data", error);
+    return new Response(JSON.stringify({ error: "Failed to post user data" }), {
+      status: 500,
+    });
+  }
+}
